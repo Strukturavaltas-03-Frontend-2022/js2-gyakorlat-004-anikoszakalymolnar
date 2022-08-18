6057f946493bd5652779b438864da6a1e89e9eb6
anikoszakalymolnar <aniko.szakaly.molnar@gmail.com>
2022-08-18 12:36:09 +0200

    solution_tested

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,5 +1,27 @@
+/* Fájl: solutions/app1.js
+Függvény neve: getSecondsOfThisYearUntilNow
+Írj egy függvényt, amelynek a visszatérési értéke az adott év 01. 01-től 08. 31-ig eltelt másodperceinek a száma!
+Kerekítést használj, amikor osztasz 1000-rel! (Math.round()) */
 
 
+/* function getSecondsOfThisYearUntilNow() {
+    const beginTime = new Date('2022.01.01.');
+    const endTime = new Date('2022.08.31');
+    return Math.round((endTime.getTime() - beginTime.getTime()) / 1000);
+}
+ */
 
+function getSecondsOfThisYearUntilNow() {
+    const beginTime = new Date('2000.01.01.');
+    const endTime = new Date('2000.08.31');
 
-// export default getSecondsOfThisYearUntilNow;
+    const now = new Date();
+    const year = now.getFullYear();
+
+    beginTime.setFullYear(year);
+    endTime.setFullYear(year);
+
+    return Math.round((endTime.getTime() - beginTime.getTime()) / 1000);
+}
+
+export default getSecondsOfThisYearUntilNow;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,28 @@
+/* Fájl: solutions/app2.js
+Függvény neve: countOfWorkingDays
+Írj egy függvényt, amelynek a visszatérési értéke az adott évben 08. 31-ig eltelt munkanapok (hétfő-péntek) száma! */
 
 
+function countOfWorkingDays() {
+    const beginTime = new Date('2000.01.01.');
+    const endTime = new Date('2000.08.31');
+
+    const now = new Date();
+    const year = now.getFullYear();
+
+    beginTime.setFullYear(year);
+    endTime.setFullYear(year);
+
+    let workdays = 0;
+
+    while (endTime.getTime() >= beginTime.getTime()) {
+        const dayType = beginTime.getDay();
+        if (dayType != 0 && dayType != 6) workdays++;
+        beginTime.setDate(beginTime.getDate() + 1);
+    }
+
+    return workdays;
+}
 
 
-
-
-// export default countOfWorkingDays;
+export default countOfWorkingDays;
